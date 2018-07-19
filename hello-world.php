24fd020abf6855d080d080a930a52f419ab89e32
Abdullahi Jama <abdjam05@student.wintec.ac.nz>
2018-07-19 00:58:01 +0000

    First commit

---
+++ hello-world.php
@@ -0,0 +1,12 @@
+<html><body>
+<h1> Hello from ABDI</h1>
+<?php
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
+
+
+
+?>
+</body>
+</html>
